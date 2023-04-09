c31e0070dc542fefb28f861e55468a82f2109f15
Humberto de Oliveira <humbertosilva06@gmail.com>
2023-04-09 14:12:14 -0300

    pratica guiada e fixação

--- database/pratica-aprofundamento-sql.sql
+++ database/pratica-aprofundamento-sql.sql
@@ -1,3 +1,4 @@
+-- Active: 1681048382860@@127.0.0.1@3306
 -- Conecte o arquivo pratica-aprofundamento-sql.db com a extensão MySQL e ative a conexão aqui
 
 -- Deletar tabela
@@ -40,6 +41,57 @@ VALUES
     (9, "blastoise", "water", 79, 83, 100, 85, 105, 78);
 
 -- Buscar todos os pokemons
-SELECT * FROM pokemons;
+SELECT * FROM pokemons 
 
 -- Práticas
+
+--1.1 pokemons com attack maior que 60--
+
+SELECT * FROM pokemons 
+WHERE attack > 60;
+
+--1.2 pokemons com attack ou special_attack maior que 60 --
+
+SELECT * FROM pokemons 
+WHERE attack >= 60 
+AND special_attack >= 60;
+
+--1.3 pokemons que tenham na coluna name o termo "saur" NO FINAL do texto
+
+SELECT * FROM pokemons
+WHERE name  LIKE "%saur";
+
+--2.1 media simples da coluna hp
+
+SELECT AVG(hp) as mediaSimplesHP
+FROM pokemons;
+
+--2.2 buscar numero de linhas
+SELECT COUNT(*) as totalDeLinhas
+FROM pokemons;
+
+
+--3.1- todos os pokemons baseado na coluna defense em ordem decrescente
+SELECT * FROM pokemons
+ORDER BY defense DESC;
+
+-- 3.2 todos os pokemons agrupados por type
+SELECT COUNT(*), type
+FROM pokemons
+GROUP BY type;
+-- todos os pokemons, resultado limitado a 3 linhas começando pela quinta
+--obs: OFFSET sempre coloque um numero antes, se quero começar a partir do 5, offset 4
+
+SELECT * FROM pokemons
+LIMIT 3
+OFFSET 4;
+
+-- FIXAÇÃO --
+
+SELECT * FROM pokemons
+WHERE type = "fire" OR type = "grass"
+ORDER BY attack
+LIMIT 3
+OFFSET 2;
+
+
